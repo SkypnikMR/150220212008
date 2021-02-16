2e6138cc8e8787c17498178c0744ba904ecb0073
SkripnikProgram <skripnikmrw@gmail.com>
2021-02-16 17:06:25 +0200

    create ReviewInfo , work with this component css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import classes from "./App.module.css";
-import heart from "./img/heart.png";
-import comment from "./img/comment.png";
 import Comment from "./Comment/Comment";
 import Profile from "./Profile/Profile";
 import Service from './Service/Service';
+import ReviewInfo from './ReviewInfo/ReviewInfo'
 //150220212008
 
 function App() {
@@ -11,24 +10,7 @@ function App() {
     <div className={classes.wrapper}>
       <Profile />
       <Service />
-      <div className={classes.info3}>
-        <div>
-          <h3>Последние отзывы</h3>
-        </div>
-        <div>
-          <h3>
-            <a href="#">Все отзывы</a>
-          </h3>
-        </div>
-        <div>
-          {" "}
-          <img src={heart} /> <p>131</p>
-        </div>
-        <div>
-          {" "}
-          <img src={comment} /> <p>14</p>
-        </div>
-      </div>
+      <ReviewInfo />
       <Comment />
       <div className={classes.InputArea}></div>
     </div>
---
+++ src/ReviewInfo/ReviewInfo.jsx
@@ -0,0 +1,25 @@
+import classes from './ReviewInfo.module.css'
+import like from '../img/like.svg'
+import comment from '../img/comment.svg'
+
+
+const ReviewInfo = (props) => {
+    return (
+
+        <div className={classes.wrapper}>
+            <div className={classes.first}>
+                <p>Последние отзывы</p>
+            </div>
+            <div className={classes.second}>
+                <a href="#">Все отзывы</a>
+            </div>
+            <div className={classes.third}>
+                <img src={like}/><p>131</p>
+            </div>
+            <div className={classes.four}>
+            <img src={comment}/><p>14</p>
+            </div>
+        </div>
+    )
+}
+export default ReviewInfo;
