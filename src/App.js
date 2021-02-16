8667e12bfef2c5fc80083c851b610cfc94bf869b
SkripnikProgram <skripnikmrw@gmail.com>
2021-02-16 05:31:49 +0200

    add Profile Component (info about girl, avatar) ;

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import classes from "./App.module.css";
-import logo from "./img/logo.png";
 import heart from "./img/heart.png";
 import comment from "./img/comment.png";
 import Comment from "./Comment/Comment";
+import Profile from './Profile/Profile';
 
 //150220212008
 
 function App() {
   return (
     <div className={classes.wrapper}>
-      <div className={classes.first}>
-        <div className={classes.avatar}>
-          <img src={logo} />
-        </div>
-        <div className={classes.profileInfo}>
-          <h1>Вероника Ростова</h1>
-          <h3>Менеджер по продажам</h3>
-          <p>
-            Подберу для вас самые лучшие предложения, мои услуги абсолютно
-            бесплатны.
-          </p>
-        </div>
-      </div>
+      <Profile/>
       <div className={classes.info2}>
         <div className={classes.services1}>
           <p>Услуг</p>
@@ -33,7 +21,7 @@ function App() {
         <div className={classes.services3}>
           <p className={classes.blueLong}>Пакетные туры</p>
           <p>3</p>
-          </div>
+        </div>
         <div className={classes.services4}>
           <p className={classes.blueShirt}>Отели</p>
           <p>1</p>
@@ -44,12 +32,24 @@ function App() {
         </div>
       </div>
       <div className={classes.info3}>
-        <div><h3>Последние отзывы</h3></div>
-        <div><h3><a href="#">Все отзывы</a></h3></div>
-        <div>  <img src={heart}/> <p>131</p></div>
-        <div> <img src={comment} /> <p>14</p></div>
+        <div>
+          <h3>Последние отзывы</h3>
+        </div>
+        <div>
+          <h3>
+            <a href="#">Все отзывы</a>
+          </h3>
+        </div>
+        <div>
+          {" "}
+          <img src={heart} /> <p>131</p>
+        </div>
+        <div>
+          {" "}
+          <img src={comment} /> <p>14</p>
+        </div>
       </div>
-      <Comment/>
+      <Comment />
       <div className={classes.InputArea}></div>
     </div>
   );
---
+++ src/Profile/Profile.jsx
@@ -0,0 +1,25 @@
+import classes from './Profile.module.css'
+import logo from "../img/logo.png";
+
+const Profile = (props) => {
+    return <div className={classes.wrapper}>
+    
+        <div className={classes.profileInfo}>
+            <div className={classes.name}>
+                <p>Вероника Ростова</p>
+            </div>
+            <div className={classes.proffesion}>
+                <p>Менеджер по продажам</p>
+            </div>
+            <div className={classes.info}>
+                <p>
+                    Подберу для вас самые лучшие предложения, мои услуги абсолютно
+                    бесплатны.
+                </p>
+            </div>
+        </div>
+        <div className={classes.avatar}/>
+        
+    </div>
+}
+export default Profile;
