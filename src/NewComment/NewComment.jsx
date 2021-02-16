e96e41ed19396faba2201eff0d1e087425173bca
SkripnikProgram <skripnikmrw@gmail.com>
2021-02-16 22:23:26 +0200

    Added adaptability for screens 320, 375, 425 and more width

--- src/NewComment/NewComment.jsx
+++ src/NewComment/NewComment.jsx
@@ -5,23 +5,23 @@ import React from 'react'
 const NewComment = ({ addComment }) => {
     const [value, setValue] = React.useState('')
     let submitHandler = (event) => {
-        let e = event || e;
         event.preventDefault()
-        if(value !== ''){
+        if (value !== '') {
             addComment(value)
             setValue('')
         }
         else return 0;
+
     }
     return (
         <form onSubmit={submitHandler} className={classes.wrapper}>
-        <div className={classes.textInput}> <input 
-        type="text" 
-        value={value}
-        onChange={event => setValue(event.target.value)}
-        /></div>
-        <div className={classes.button}><button type="submit">Написать консультанту</button></div>
-    </form>
+            <div className={classes.textInput}> <input
+                type="text"
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            /></div>
+            <div className={classes.button}><button type="submit">Написать консультанту</button></div>
+        </form>
     )
 }
 
