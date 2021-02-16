a3930411ce76ce3e4537bfa8f22429fd9000a796
SkripnikProgram <skripnikmrw@gmail.com>
2021-02-16 17:45:29 +0200

    do better css to Comment

--- src/Comment/Comment.jsx
+++ src/Comment/Comment.jsx
@@ -3,16 +3,10 @@ import classes from './Comment.module.css';
 const Comment = (props) => {
     return (
         <div className={classes.comment}>
-            <div className={classes.who}><h3>Сумаил</h3><p>13 октября 2011</p></div>
-            
-            <div className={classes.text}><p>Привет, Верунь! ниче себе ты крутая. фотка класс!!!!</p></div>
-
-            <div className={classes.who}><h3>Лилия Семёновна </h3><p>14 октября 2011</p></div>
-            <div className={classes.text}><p>Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент, это и есть всемирно 
+            <div className={classes.who}><p>Лилия Семеновна</p><p><span>13 октября 2011</span></p></div>
+            <div className={classes.text}><p>Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц 
+                жизненно стабилизирует кинетический момент, это и есть всемирно 
                 известный центр огранки алмазов и торговли бриллиантами?</p></div>
-
-            <div className={classes.who}><h3>Лилия Семёновна </h3><p>14 октября 2011</p></div> 
-            <div className={classes.text}><p>Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?</p></div>
 
         </div>
 
